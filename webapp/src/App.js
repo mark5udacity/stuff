fe16e42c4b1b8ff421c25577ff4f36ef658c43f8
Mark 5 <markvudacity@gmail.com>
2018-05-25 10:56:31 -0400

    [on-site] Write a simple progress bar.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,6 +4,17 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+      const { progress } = this.props;
+      console.log('PROGRESS', progress);
+
+      const styleColor = 'blue';
+      const divStyle = {
+          color: styleColor,
+          width: `${progress}%`
+      };
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +24,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+
+          <div className={"progress-bar"}>
+              <div className={"progress"} style={divStyle}>
+                  {progress}%
+              </div>
+          </div>
       </div>
     );
   }
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+for (let i = 1; i <= 10; ++i) {
+    console.log('time for: ', i);
+    setTimeout(() => {
+        console.log('rerendering for i: ', i);
+        const progress = 10 * i;
+        ReactDOM.render(<App progress={progress}/>, document.getElementById('root'));
+        },
+        i * 500
+    );
+}
+
+
 registerServiceWorker();
