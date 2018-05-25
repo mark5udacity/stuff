76e99cf4bfd5ef02711074b5c3fc0a0c22a4897c
Mark 5 <markvudacity@gmail.com>
2018-05-25 11:00:24 -0400

    [on-site] First attempt to add a button to add multiple progress bars (out of time).

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function ProgressElem(props) {
+    const { progress } = props;
+    console.log('in processElem fn', progress);
+    const divStyle = {
+        color: 'blue',
+        width: `${progress}%`
+    };
+
+    return <div className={"progress-bar"}>
+        <div className={"progress"} style={divStyle}>
+            {progress}%
+        </div>
+    </div>;
+}
+
 class App extends Component {
+    
   render() {
 
-      const { progress } = this.props;
-      console.log('PROGRESS', progress);
+      let progress = 'CLick me!';
 
-      const styleColor = 'blue';
-      const divStyle = {
-          color: styleColor,
-          width: `${progress}%`
+      const buttonClick = () => {
+          for (let i = 1; i <= 10; ++i) {
+              console.log('time for: ', i);
+              setTimeout(() => {
+                      console.log('rerendering for i: ', i);
+                      const prog = 10 * i;
+                      progress = <ProgressElem progress={prog}/>
+                  },
+                  i * 500
+              );
+          }
       };
 
 
@@ -25,12 +47,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
+          <button onClick={buttonClick}>{progress}</button>
 
-          <div className={"progress-bar"}>
-              <div className={"progress"} style={divStyle}>
-                  {progress}%
-              </div>
-          </div>
       </div>
     );
   }
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -5,16 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 
-for (let i = 1; i <= 10; ++i) {
-    console.log('time for: ', i);
-    setTimeout(() => {
-        console.log('rerendering for i: ', i);
-        const progress = 10 * i;
-        ReactDOM.render(<App progress={progress}/>, document.getElementById('root'));
-        },
-        i * 500
-    );
-}
-
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
